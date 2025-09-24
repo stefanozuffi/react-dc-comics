cefdba48f9421dbae644c300bc229c3ba3d80b1d
stefanozuffi <stefano.zuffi06@outlook.com>
2025-09-24 17:58:16 +0200

    header

---
+++ react-dc-comics/src/assets/components/AppHeader.jsx
@@ -0,0 +1,22 @@
+import DC_Logo from '../img/dc-logo.png'
+
+export default function AppHeader() {
+    return (
+        <header>
+        <img src={DC_Logo} alt="Small-Logo"/>
+        <nav>
+            <a href="#">CHARACTERS</a>
+            <a href="#" className="active">COMICS</a>
+            <a href="#">MOVIES</a>
+            <a href="#">TV</a>
+            <a href="#">GAMES</a>
+            <a href="#">COLLECTIBLES</a>
+            <a href="#">VIDEOS</a>
+            <a href="#">FANS</a>
+            <a href="#">NEWS</a>
+            <a href="#">SHOPS</a>
+        </nav>
+    </header>
+    )
+   
+}
