1c80565c41f1b96803b10a248fffd137e6f9c6fb
stefanozuffi <stefano.zuffi06@outlook.com>
2025-09-24 18:00:54 +0200

    main

--- react-dc-comics/src/App.jsx
+++ react-dc-comics/src/App.jsx
@@ -2,12 +2,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 import AppHeader from './assets/components/AppHeader.jsx'
+import AppMain from './assets/components/AppMain.jsx'
 
 function App() {
 
   return (
     <>
     <AppHeader/>
+    <AppMain/>
 
     </>
   )
---
+++ react-dc-comics/src/assets/components/AppMain.jsx
@@ -0,0 +1,7 @@
+export default function AppMain() {
+    return (
+        <main>
+            CONTENT GOES HERE
+        </main>
+    )
+}
